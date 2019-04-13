550386ce3b4a9c6937f56b5d1ea228a64ad35028
Bidur_Banstola <bidur.banstola@gmail.com>
2019-04-13 20:55:42 +0545

    Week 3

--- Week3/build.sbt
+++ Week3/build.sbt
@@ -11,3 +11,5 @@ libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
 // https://mvnrepository.com/artifact/junit/junit
 libraryDependencies += "junit" % "junit" % "4.12" % Test
 
+// libraryDependencies ++= Seq("com.typesafe.play" % "play-json_2.11" % "2.4.2")
+
--- Week3/src/main/scala/objsets/TweetSet.scala
+++ Week3/src/main/scala/objsets/TweetSet.scala
@@ -55,6 +55,95 @@ class Empty extends TweetSet {
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet{
 
+  override def mostRetweeted:Tweet = {
+    val leftBiggest = if (!left.isEmpty) left.mostRetweeted else elem
+    val rightBiggest = if (!right.isEmpty) right.mostRetweeted else elem
+    val bigger = if (leftBiggest.retweets > rightBiggest.retweets) leftBiggest else rightBiggest
+    if (elem.retweets > bigger.retweets) elem else bigger
+  }
+
+  override def isEmpty:Boolean = false
+
+  override def descendingByRetweet:TweetList = {
+    descendingByRetweetAcc(this, Nil)
+  }
+
+  def descendingByRetweetAcc(space: TweetSet, acc:TweetList):TweetList ={
+    if (space.isEmpty) acc else {
+      val biggest = space.mostRetweeted
+      val newAcc = descendingByRetweetAcc(space.remove(biggest), acc)
+      new Cons(biggest, newAcc)
+    }
+  }
+
+  def contains(x: Tweet): Boolean =
+    if (x.text < elem.text) left.contains(x)
+    else if (elem.text < x.text) right.contains(x)
+    else true
+
+  def incl(x: Tweet): TweetSet = {
+    if (x.text < elem.text) new NonEmpty(elem, left.incl(x), right)
+    else if (elem.text < x.text) new NonEmpty(elem, left, right.incl(x))
+    else this
+  }
+
+  def remove(tw: Tweet): TweetSet =
+    if (tw.text < elem.text) new NonEmpty(elem, left.remove(tw), right)
+    else if (elem.text < tw.text) new NonEmpty(elem, left, right.remove(tw))
+    else left.union(right)
+
+  def foreach(f: Tweet => Unit): Unit = {
+    f(elem)
+    left.foreach(f)
+    right.foreach(f)
+  }
+}
+
+trait TweetList {
+  def head: Tweet
+  def tail: TweetList
+  def isEmpty: Boolean
+  def foreach(f: Tweet => Unit): Unit =
+    if (!isEmpty) {
+      f(head)
+      tail.foreach(f)
+    }
+}
+
+object Nil extends TweetList {
+  def head = throw new java.util.NoSuchElementException("head of EmptyList")
+  def tail = throw new java.util.NoSuchElementException("tail of EmptyList")
+  def isEmpty = true
+}
+
+class Cons(val head: Tweet, val tail: TweetList) extends TweetList {
+  def isEmpty = false
+}
+
+object GoogleVsApple {
+  val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
+  val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
+
+  def containsKeywords(tw: Tweet, keywords: List[String]): Boolean = {
+    if (keywords.isEmpty) false else {
+      if (tw.text.contains(keywords.head)) true else containsKeywords(tw, keywords.tail)
+    }
+  }
+
+  def findTweets(allTweets: TweetSet, keywords: List[String]): TweetSet = {
+    if (keywords.isEmpty) allTweets else {
+      allTweets.filter(tw => containsKeywords(tw, keywords))
+    }
+  }
+
+  lazy val googleTweets: TweetSet = findTweets(TweetReader.allTweets, google)
+  lazy val appleTweets: TweetSet = findTweets(TweetReader.allTweets, apple)
+
+  lazy val trending: TweetList = googleTweets.union(appleTweets).descendingByRetweet
+}
+
+object Main extends App {
+  GoogleVsApple.trending foreach println
 }
 
 
